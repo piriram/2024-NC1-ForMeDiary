79e74424cf74beb7ee5ba0256e54cd845dfd9e8e
piri <pyoram25@gmail.com>
2024-04-14 15:48:23 +0900

    commit

---
+++ NC1/View/CreateTopView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateTopView.swift
+//  NC1
+//
+//  Created by ram on 4/14/24.
+//
+
+import SwiftUI
+
+struct CreateTopView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateTopView()
+}
