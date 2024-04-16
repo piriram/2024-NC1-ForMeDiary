fc3ed7be35a189075f59f6fdeff53a8d58789882
piri <pyoram25@gmail.com>
2024-04-16 14:04:41 +0900

    수정창 만드는중

---
+++ NC1/View/EmotionView.swift
@@ -0,0 +1,18 @@
+//
+//  EmotionView.swift
+//  NC1
+//
+//  Created by ram on 4/16/24.
+//
+
+import SwiftUI
+
+struct EmotionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmotionView()
+}
---
+++ NC1/View/ListDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ListDetailView.swift
+//  NC1
+//
+//  Created by ram on 4/16/24.
+//
+
+import SwiftUI
+
+struct ListDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListDetailView()
+}
